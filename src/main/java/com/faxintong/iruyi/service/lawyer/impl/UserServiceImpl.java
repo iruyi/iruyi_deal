c33ff9712246f07af4023044974746a72a83e3fb
刘明 <liuming0430@gmail.com>
2015-01-09 12:27:33 +0800

    login and register

--- src/main/java/com/faxintong/iruyi/service/lawyer/impl/UserServiceImpl.java
+++ src/main/java/com/faxintong/iruyi/service/lawyer/impl/UserServiceImpl.java
@@ -1,26 +1,42 @@
 package com.faxintong.iruyi.service.lawyer.impl;
 
+import com.faxintong.iruyi.dao.mybatis.lawyer.LawyerMapper;
 import com.faxintong.iruyi.model.mybatis.lawyer.Lawyer;
+import com.faxintong.iruyi.model.mybatis.lawyer.LawyerExample;
 import com.faxintong.iruyi.service.lawyer.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by hehj on 15-1-3.
  */
-@Service
+@Service("userService")
 public class UserServiceImpl implements UserService {
+    @Autowired
+    private LawyerMapper lawyerMapper;
+
     @Override
     public void registerLawyer(Lawyer lawyer) throws Exception {
-
+        lawyerMapper.insertSelective(lawyer);
     }
 
     @Override
     public boolean regisValidate(String phone) throws Exception {
-        return false;
+        return phone.matches("^1[0-9]{10}$");
     }
 
     @Override
     public boolean loginValidate(String loginName, String password) throws Exception {
-        return false;
+        LawyerExample lawyerExample = new LawyerExample();
+        lawyerExample.createCriteria().andNameEqualTo(loginName);
+        return lawyerMapper.selectByExample(lawyerExample).size() == 1;
+    }
+
+    @Override
+    public boolean containsPhone(String phone) throws Exception {
+        LawyerExample lawyerExample = new LawyerExample();
+        lawyerExample.createCriteria().andPhoneEqualTo(phone);
+        return lawyerMapper.selectByExample(lawyerExample).size() == 1;
     }
 }
