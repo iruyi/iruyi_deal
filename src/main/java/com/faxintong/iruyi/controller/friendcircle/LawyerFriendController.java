98f94ea9951ed0c916e0bb705cf995e13346a996
刘明 <liuming0430@gmail.com>
2015-01-19 17:19:29 +0800

    添加好友Controller层的校验

--- src/main/java/com/faxintong/iruyi/controller/friendcircle/LawyerFriendController.java
+++ src/main/java/com/faxintong/iruyi/controller/friendcircle/LawyerFriendController.java
@@ -3,14 +3,18 @@ package com.faxintong.iruyi.controller.friendcircle;
 import com.faxintong.iruyi.model.mybatis.lawyer.Lawyer;
 import com.faxintong.iruyi.service.friendcircle.FriendsService;
 import com.google.common.collect.Maps;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Map;
 
@@ -27,62 +31,74 @@ public class LawyerFriendController {
     @Autowired
     private FriendsService friendsService;
 
-    @RequestMapping(value = "search")
+    @RequestMapping(value = "search", method = RequestMethod.GET)
     public Map<String, Object> searchLawyer(String name, String phone, HttpServletResponse response){
         Map<String, Object> result = Maps.newHashMap();
         result.put(RESULT, false);
         List<Lawyer> lawyerList = null;
         try {
-            lawyerList = friendsService.searchLawyer(name, phone);
+            if(!StringUtils.isEmpty(name) || !StringUtils.isEmpty(phone))
+                lawyerList = friendsService.searchLawyer(name, phone);
             result.put("lawyerList", lawyerList);
+            result.put(RESULT, true);
         }catch (Exception e){
-            logger.debug("根据名字电话查找律师失败, 名字:" + name + " 电话:" + phone);
+            logger.error("根据名字电话查找律师失败, 名字:" + name + " 电话:" + phone);
         }
         return result;
     };
 
     @RequestMapping(value = "add")
-    public Map<String, Object> addSearchFriends(Long lawyerId, Long friendId,
+    public Map<String, Object> addSearchFriends(@NotNull Long lawyerId,@NotNull Long friendId, BindingResult bindingResult,
                                                 HttpServletRequest request, HttpServletResponse response){
         Map<String, Object> result = Maps.newHashMap();
         result.put(RESULT, false);
+        if (bindingResult.hasErrors()) {
+            result.put(ERR_MSG, bindingResult.getFieldError());
+            return result;
+        }
         try {
             friendsService.addSearchFriends(lawyerId, friendId);
             result.put(RESULT, true);
         } catch (Exception e) {
-            logger.debug("添加好友失败:" + e.getMessage());
+            logger.error("添加好友失败:" + e.getMessage());
         }
         return result;
     }
 
     @RequestMapping(value = "confirm")
-    public Map<String, Object> friendConfirm(Long id){
+    public Map<String, Object> friendConfirm(@NotNull Long id, BindingResult bindingResult,
+                                             HttpServletRequest request, HttpServletResponse response){
         Map<String, Object> result = Maps.newHashMap();
         result.put(RESULT, false);
-        if (id != null){
-            try {
-                friendsService.friendConfirm(id);
-                result.put(RESULT, true);
-            }catch (Exception e){
-                logger.debug("确认朋友关系失败:" + e.getMessage());
-            }
+        if(bindingResult.hasErrors()){
+            result.put(ERR_MSG, bindingResult.getFieldError());
+            return result;
+        }
+        try {
+            friendsService.friendConfirm(id);
+            result.put(RESULT, true);
+        }catch (Exception e){
+            logger.error("确认朋友关系失败:" + e.getMessage());
         }
         return result;
     }
 
     @RequestMapping(value = "find")
-    public Map<String, Object> findFriends(Long lawyerId){
+    public Map<String, Object> findFriends(@NotNull Long lawyerId,BindingResult bindingResult,
+                                           HttpServletRequest request, HttpServletResponse response){
         Map<String, Object> result = Maps.newHashMap();
         result.put(RESULT, false);
         List<Lawyer> lawyerList  = null;
+        if (bindingResult.hasErrors()){
+            result.put(ERR_MSG, bindingResult.getFieldError());
+            return result;
+        }
         try {
-            if (lawyerId != null) {
-                lawyerList = friendsService.findFriends(lawyerId);
-                result.put("lawyerList", lawyerList);
-                result.put(RESULT, true);
-            }
+            lawyerList = friendsService.findFriends(lawyerId);
+            result.put("lawyerList", lawyerList);
+            result.put(RESULT, true);
         }catch (Exception e){
-            e.printStackTrace();
+            logger.error("查找朋友失败:" + e.getMessage());
         }
         return result;
     }
