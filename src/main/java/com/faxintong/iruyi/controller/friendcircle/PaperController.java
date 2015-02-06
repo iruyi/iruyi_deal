d3731dd7f40a770e57e341c45aa0753ef0eb8c31
刘明 <liuming0430@gmail.com>
2015-02-06 12:58:31 +0800

    律师提问调试

--- src/main/java/com/faxintong/iruyi/controller/friendcircle/PaperController.java
+++ src/main/java/com/faxintong/iruyi/controller/friendcircle/PaperController.java
@@ -5,6 +5,7 @@ import com.faxintong.iruyi.model.mybatis.friendcircle.Paper;
 import com.faxintong.iruyi.model.mybatis.lawyer.Lawyer;
 import com.faxintong.iruyi.service.friendcircle.PaperService;
 import com.google.common.collect.Maps;
+import org.apache.commons.lang.StringUtils;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -52,24 +53,32 @@ public class PaperController extends BaseController{
             paperService.reportPaper(paper);
             result.put(RESULT, true);
         } catch (Exception e) {
+            e.printStackTrace();
             logger.error("发表文章失败:" + e.getMessage());
         }
         return result;
     }
 
     @RequestMapping(value = "delete", method = RequestMethod.POST)
-    public Map<String, Object> deletePaperById(@NotEmpty Long paperId, BindingResult bindingResult,
+    public Map<String, Object> deletePaperById(Long paperId,
                                                HttpServletRequest request, HttpServletResponse response){
         Map<String , Object> result = Maps.newHashMap();
         result.put(RESULT, false);
-        if(bindingResult.hasErrors()){
-            result.put(ERR_MSG, bindingResult.getFieldError().getDefaultMessage());
+        if(paperId == null){
+            result.put(ERR_MSG, "提问ID不能为空");
             return result;
         }
         try {
-            paperService.deletePaperById(paperId);
-            result.put(RESULT, true);
+            Long lawyerId = getLawyerId(request);
+            Paper paper = paperService.findPaperById(paperId);
+            if(paper != null && paper.getLawyerId() == lawyerId) {
+                paperService.deletePaperById(paperId);
+                result.put(RESULT, true);
+            }else{
+                result.put(ERR_MSG, "提问只能由提问律师删除！");
+            }
         } catch (Exception e) {
+            e.printStackTrace();
             logger.error("删除文章失败:" + e.getMessage());
         }
         return result;
@@ -84,97 +93,120 @@ public class PaperController extends BaseController{
             result.put("paper", paperService.findPaperByLawyerId(lawyerId));
             result.put(RESULT, true);
         }catch (Exception e){
+            e.printStackTrace();
             logger.error("根据律师ID查找文章失败:" + e.getMessage());
         }
         return result;
     }
 
     @RequestMapping(value = "findPaper",  method = RequestMethod.GET)
-    public Map<String, Object> findPaperById(@NotEmpty Long paperId, BindingResult bindingResult,
+    public Map<String, Object> findPaperById(Long paperId,
                                              HttpServletRequest request, HttpServletResponse response){
         Map<String , Object> result = Maps.newHashMap();
         result.put(RESULT, false);
-        if(bindingResult.hasErrors()){
-            result.put(ERR_MSG, bindingResult.getFieldError().getDefaultMessage());
+        if(paperId == null){
+            result.put(ERR_MSG, "提问ID不能为空");
             return result;
         }
         try {
             result.put("paper", paperService.findPaperById(paperId));
         }catch (Exception e){
+            e.printStackTrace();
             logger.error("根据文章ID查找文章失败:" + e.getMessage());
         }
         return result;
     }
 
+    /**
+     * 发表评论
+     * @param paperId
+     * @param comment
+     * @param request
+     * @param response
+     * @return
+     */
     @RequestMapping(value = "comment/report", method = RequestMethod.POST)
-    public Map<String, Object> reportPaperComment(@NotEmpty Long paperId, @NotEmpty String comment,BindingResult bindingResult,
+    public Map<String, Object> reportPaperComment(Long paperId, String comment,
                                                   HttpServletRequest request,HttpServletResponse response){
         Map<String , Object> result = Maps.newHashMap();
         result.put(RESULT, false);
-        if(bindingResult.hasErrors()){
-            result.put(ERR_MSG, bindingResult.getFieldError().getDefaultMessage());
+
+        if(paperId == null){
+            result.put(ERR_MSG, "提问ID不能为空");
             return result;
         }
+
+        if(StringUtils.isEmpty(comment)){
+            result.put(ERR_MSG, "评论不能为空");
+            return result;
+        }
+
         Long lawyerId = getLawyerId(request);
         try {
             paperService.reportPaperComment(lawyerId, paperId, comment);
             result.put(RESULT, true);
         }catch (Exception e){
+            e.printStackTrace();
             logger.error("发表评论失败:" + e.getMessage());
         }
         return result;
     }
 
     @RequestMapping(value = "praise", method = RequestMethod.POST)
-    public Map<String , Object> praisePaper(@NotEmpty Long paperId, BindingResult bindingResult,
+    public Map<String , Object> praisePaper(Long paperId,
                                             HttpServletRequest request, HttpServletResponse response) {
         Map<String , Object> result = Maps.newHashMap();
         result.put(RESULT, false);
-        if(bindingResult.hasErrors()){
-            result.put(ERR_MSG, bindingResult.getFieldError().getDefaultMessage());
+
+        if(paperId == null){
+            result.put(ERR_MSG, "提问id不能为空");
             return result;
         }
-        Lawyer lawyer = getLawyer(request);
+
         try {
+            Lawyer lawyer = getLawyer(request);
             paperService.praisePaper(lawyer.getId(), lawyer.getName(), paperId);
             result.put(RESULT, true);
         } catch (Exception e) {
+            e.printStackTrace();
             logger.error("赞失败:" + e.getMessage());
         }
         return result;
     }
 
     @RequestMapping(value = "findPaperComments", method = RequestMethod.GET)
-    public Map<String , Object> findPaperComments(@NotEmpty Long paperId, BindingResult bindingResult,
+    public Map<String , Object> findPaperComments(Long paperId,
                                                   HttpServletRequest request, HttpServletResponse response){
         Map<String , Object> result = Maps.newHashMap();
         result.put(RESULT, false);
-        if(bindingResult.hasErrors()){
-            result.put(ERR_MSG, bindingResult.getFieldError().getDefaultMessage());
+        if(paperId == null){
+            result.put(ERR_MSG, "提问ID不能为空");
             return result;
         }
         try {
             result.put("comments", paperService.findPaperComments(paperId));
             result.put(RESULT, true);
         } catch (Exception e) {
+            e.printStackTrace();
             logger.error("查找文章评论失败:" + e.getMessage());
         }
         return result;
     }
 
     @RequestMapping(value = "findPaperPraises",  method = RequestMethod.GET)
-    public Map<String , Object> findPaperPraises(@NotEmpty Long paperId, BindingResult bindingResult,
+    public Map<String , Object> findPaperPraises(Long paperId,
                                                  HttpServletRequest request, HttpServletResponse response){
         Map<String, Object> result = Maps.newHashMap();
         result.put(RESULT, false);
-        if(bindingResult.hasErrors()){
-            result.put(ERR_MSG, bindingResult.getFieldError().getDefaultMessage());
+        if(paperId == null){
+            result.put(ERR_MSG, "提问ID不能为空");
             return result;
         }
         try {
             result.put("praise", paperService.findPaperPraises(paperId));
             result.put(RESULT, true);
         } catch (Exception e) {
+            e.printStackTrace();
             logger.error("查找文章赞失败:" + e.getMessage());
         }
         return result;
