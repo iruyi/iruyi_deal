b635a6ead8bb58d317d43c21d631d6ddef7c5f96
hehongju <879799617@qq.com>
2015-02-10 23:19:19 +0800

    生成问题点赞model

---
+++ src/main/java/com/faxintong/iruyi/model/mybatis/community/ReplyPraise.java
@@ -0,0 +1,45 @@
+package com.faxintong.iruyi.model.mybatis.community;
+
+import java.util.Date;
+
+public class ReplyPraise {
+    private Long id;
+
+    private Long replyId;
+
+    private Long lawyerId;
+
+    private Date createDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getReplyId() {
+        return replyId;
+    }
+
+    public void setReplyId(Long replyId) {
+        this.replyId = replyId;
+    }
+
+    public Long getLawyerId() {
+        return lawyerId;
+    }
+
+    public void setLawyerId(Long lawyerId) {
+        this.lawyerId = lawyerId;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
