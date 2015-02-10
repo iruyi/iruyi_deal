04f712e32ee8f5b2d9b2e9580a6b1b51f0595ce4
刘明 <liuming0430@gmail.com>
2015-02-10 23:09:00 +0800

    社区

---
+++ src/main/java/com/faxintong/iruyi/controller/community/CommunityController.java
@@ -0,0 +1,13 @@
+package com.faxintong.iruyi.controller.community;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ron on 2015/2/10.
+ */
+@RestController
+@RequestMapping("community")
+public class CommunityController {
+
+}
