b7437f3e205ec447f948ce96ac783da48a8baa91
bochenlong <bochenlong>
2015-04-19 16:11:11 +0800

    投票代码

--- src/main/java/com/faxintong/iruyi/dao/mybatis/vote/VoteMapper.java
+++ src/main/java/com/faxintong/iruyi/dao/mybatis/vote/VoteMapper.java
@@ -2,12 +2,27 @@ package com.faxintong.iruyi.dao.mybatis.vote;
 
 import com.faxintong.iruyi.model.mybatis.vote.Vote;
 import com.faxintong.iruyi.model.mybatis.vote.VoteExample;
+import com.faxintong.iruyi.model.mybatis.vote.vo.VotehVoteOptionVo;
 import com.faxintong.iruyi.operate.OperateMyBatis;
+import com.faxintong.iruyi.utils.Pager;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
+
 @OperateMyBatis
 public interface VoteMapper {
+    /**投票操作*/
+    int insertVoteResult(Long voteId, Long optionId, Long lawyerId);
+    /**获取调查列表*/
+    List<VotehVoteOptionVo> selectVotehVoteOption(Pager pager);
+    /**获取投票总数*/
+    int countVoteOption(Long voteId);
+    /**获取各个项投票总数*/
+    Map<Integer,Integer> countVoteOptionGroupByOption(Long voteId);
+
+
+
     int countByExample(VoteExample example);
 
     int deleteByExample(VoteExample example);
--- src/main/java/com/faxintong/iruyi/service/vote/impl/VoteServiceImpl.java
+++ src/main/java/com/faxintong/iruyi/service/vote/impl/VoteServiceImpl.java
@@ -1,8 +1,11 @@
 package com.faxintong.iruyi.service.vote.impl;
 
+import com.faxintong.iruyi.dao.mybatis.vote.VoteMapper;
 import com.faxintong.iruyi.model.mybatis.vote.Vote;
+import com.faxintong.iruyi.model.mybatis.vote.vo.VotehVoteOptionVo;
 import com.faxintong.iruyi.service.vote.VoteService;
 import com.faxintong.iruyi.utils.Pager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,18 +15,24 @@ import java.util.List;
  */
 @Service
 public class VoteServiceImpl implements VoteService {
+    @Autowired
+    private VoteMapper voteMapper;
+
     @Override
-    public List<Vote> getVoteList(Pager pager) throws Exception {
-        return null;
+    public List<VotehVoteOptionVo> getVoteList(Pager pager) throws Exception {
+        return voteMapper.selectVotehVoteOption(pager);
     }
 
     @Override
     public Vote voteDetail(Long voteId) throws Exception {
-        return null;
+        VotehVoteOptionVo votehVoteOptionVo = voteMapper.selectVotehVoteOption(null).get(0);
+        votehVoteOptionVo.setVoteNum(voteMapper.countVoteOption(voteId));
+        votehVoteOptionVo.setOptionNum(voteMapper.countVoteOptionGroupByOption(voteId));
+        return votehVoteOptionVo;
     }
 
     @Override
     public void voteChoise(Long voteId, Long optionId, Long lawyerId) throws Exception {
-
+        voteMapper.insertVoteResult(voteId,optionId,lawyerId);
     }
 }
